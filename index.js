83bbe865f348d0b3c4ca365149ca267aa7c0eb3a
Mikhail Makharia <makharia.study@gmail.com>
2024-02-26 17:15:14 +0300

    Index, make question base, safe to file

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-const rds = require("readline-sync");
+import writeQuestion from "./writeQuestion.js";
+import rds from "readline-sync";
+import fs from "fs";
 
 export const questionArray = [];
+fs.readFileSync("questions.txt", "utf-8")
+  .split("STOP")
+  .forEach((val) => {
+    const object = {};
+    if (!val) {
+      console.log("Val ended!");
+      return;
+    }
+    val.split("||").forEach((value) => {
+      if (value === "STOP" || !value) {
+        console.log("STOP!!!!");
+        return;
+      }
+      object[value.split("=")[0]] = value.split("=")[1];
+    });
+    questionArray.push(object);
+  });
+console.log(questionArray);
+export const playerArray = [];
+export let author;
 
 export default function gameProgression() {
+  console.log(
+    "---------------QQQ------------U--U------------I-----------ZZZZ--------------"
+  );
+  console.log(
+    "--------------Q---Q-----------U--U------------I-------------Z---------------"
+  );
+  console.log(
+    "--------------Q---Q-----------U--U------------I------------Z----------------"
+  );
+  console.log(
+    "---------------QQQ-Q----------UUUU------------I-----------ZZZZ--------------"
+  );
   let exitFlag = false;
   while (!exitFlag) {
+    console.log("\n \n");
     switch (
       rds.question(
-        "Interactive Quiz ver. 1.2.2\nw - write a question\nn - save your name for scoring\ns - start the game, choose the amount and start playing!\nq - quit the program\n: "
+        "Interactive Quiz ver. 1.2.2\nw - write a question\nn - input your name for scoring\ns - start the game, choose the amount and start playing!\nq - quit the program\n: "
       )
     ) {
       case "w":
+        questionArray.push(writeQuestion(author));
         break;
       case "n":
         break;
@@ -18,9 +54,32 @@ export default function gameProgression() {
         break;
       case "q":
         exitFlag = true;
+        let saveString = "";
+        if (questionArray.length === 0) {
+          return;
+        }
+        for (const object of questionArray) {
+          console.log(object);
+          Object.keys(object).forEach((element, key) => {
+            saveString = `${saveString}${element}=${
+              Object.values(object)[key]
+            }||`;
+          });
+          saveString = `${saveString}STOP`;
+        }
+        fs.writeFileSync("questions.txt", saveString, (error) => {
+          if (error) {
+            throw error;
+          } else {
+            console.log("Questions saved");
+          }
+        });
+        break;
       default:
         console.log("Invalid letter!");
     }
   }
   return;
 }
+
+gameProgression();
---
+++ writeQuestion.js
@@ -0,0 +1,52 @@
+import rds from 'readline-sync';
+
+export default function writeQuestion(author = 'Anonymous') {
+  console.log('Time to create your own question!');
+  const questionObject = {
+    category: rds.question("Category: ").toLowerCase().trim() || 'Oops...',
+    question: rds.question("Question: ").toLowerCase().trim() || 'Oops...',
+    points: +rds.question("Amount of points awarded: ") || 0,
+    author: author,
+  };
+  const type = rds.question('Choose the type!\nslider - answer is given using a number slider!\ntext - answer is written using text(non case sensitive, but has to be exact)\nanswer - answer is chosen from the provided options\n: ');
+  switch(type) {
+    case 'slider':
+      questionObject.type = 'slider';
+      console.log('Type set to slider.');
+      questionObject.correct = rds.question('Input the correct answer(number from 1 to 100): ');
+      break;
+    case 'text':
+      questionObject.type = 'text';
+      console.log('Type set to text.');
+      questionObject.correct = rds.question('Input the correct answer(is not case sensitive): ');
+      break;
+    case 'answer':
+      questionObject.type = 'answer';
+      const answerArray = [];
+      const amount = rds.question('How many answers?: ');
+      if (amount <= 0) {
+        console.log('Amount is incorrect!');
+        return;
+      }
+      for (let i = 0; i < amount; i+= 1) {
+        const answer = rds.question('Input a single asnwer! \nIf it is the right answer, follow it with " CORRECT"(note the space). \nIf several correct answers are inputted, only the first will count as the correct one, while the rest will be ignored.\n: ');
+        
+        if (answer.split(' ').slice(-1).join() === 'CORRECT') {
+          console.log('Correct answer input!');
+          questionObject.correct = answer.split(' ').slice(0, -1).join()
+          answerArray.push(answer.split(' ').slice(0, -1).join());
+        } else {
+          answerArray.push(answer);
+        }
+      }
+      questionObject.answers = answerArray;
+      console.log('Type set to answer.');
+      break;
+    default:
+      console.log('Type incorrect! Defaulting to "text".');
+      questionObject.type = 'text';
+      break;
+  }
+  console.log(questionObject);
+  return questionObject;
+}
